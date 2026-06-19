d35ee6ff354cf858b5f254cd7063fefc9f04f13e
cfbrau <braufabian@gmail.com>
2026-06-19 08:07:36 -0300

    fix/config de seguridad

--- src/main/java/com/stepside/StepSide/common/security/SecurityConfig.java
+++ src/main/java/com/stepside/StepSide/common/security/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                //.cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable()) // Stateless para APIs NoSQL
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
